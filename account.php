103fd30c900cda8f3e1b077d1146eb6c1b43e1f0
André Morais <48691769+moraisandreua@users.noreply.github.com>
2022-03-23 15:12:58 +0000

    INIT

---
+++ account.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <link href="assets/estilos/estilos.css" ref="stylesheet"/>
+    </head>
+    <body>
+        <div class="navbar">navbar</div>
+        <div class="content">login form</div>
+        <div class="footer">footer</div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="estilos.css">
+        <link rel="stylesheet" href="assets/estilos/estilos.css">
+    </head>
+    <body>
+        <div class="navbar">
+            <div class="navbarLogo">
+                <img src="assets/img/logo.png"/>
+            </div>
+            <div class="navbarWrap">
+            
+            </div>
+            <div class="navbarButtons">
+                <button class="navbarButtonsLogin">Login</button>
+                <button class="navbarButtonsSignup">Sign-up</button>
+            </div>
+        </div>
+        <div class="content">
+            <div class="contentPanel" style="background-image: url('/assets/img/panel1.jpg');">
+                <div style="display:flex; flex-direction:column; align-items:center; justify-content:center;">
+                    <div style="font-size:40pt; font-family:Exo-Regular;">Powered by NFTs</div>
+                    <div><button class="enterAppButton">Enter App</button></div>
+                </div>
+            </div>
+            <div class="contentPanel" style="align-items:top;">
+                <div style="display:flex; flex-direction:column; align-items:center; justify-content:center;">
+                    <div style="font-family:Exo-Regular;">active trader</div>
+                    <div style="font-size:40pt; font-family:Exo-Regular;">Own part of Blue-Chip NFTs</div>
+                </div>
+            </div>
+            <div class="contentPanel" style="align-items:top;">
+                <div style="display:flex; flex-direction:column; align-items:center; justify-content:center;">
+                    <div style="font-family:Exo-Regular;">in your pocket</div>
+                    <div style="font-size:40pt; font-family:Exo-Regular;">Control your stake everywhere</div>
+                </div>
+            </div>
+
+        </div>
+        <div class="footer">footer</div>
+    </body>
+</html>
